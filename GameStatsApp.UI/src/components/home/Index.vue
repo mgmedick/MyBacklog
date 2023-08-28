af6ae13e743bbe21c3516d15182385a987bf136d
mgmedick <mgmedick@live.com>
2023-08-28 11:55:20 -0500

    Includig demo images in gitignore.

--- GameStatsApp.UI/src/components/home/Index.vue
+++ GameStatsApp.UI/src/components/home/Index.vue
@@ -1,24 +1,20 @@
 ﻿<template>
     <div>
-        <div v-if="!indexvm.isAuth">          
+        <div v-if="!indexvm.isAuth">
+            <div class="text-center mb-3">
+                <h2>Welcome to GameStatsApp</h2>
+                <div>
+                    <span>A tool to keep track of your games and progress</span>
+                </div>
+            </div>
             <div id="carouselExampleDark" class="carousel carousel-dark">
                 <div class="carousel-indicators">
                     <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                     <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
                     <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="3" aria-label="Slide 4"></button>
                 </div>
-                <div class="carousel-inner">
+                <div class="carousel-inner">                    
                     <div class="carousel-item active" data-bs-interval="10000">
-                        <div style="font-size: 75px;">
-                            <font-awesome-icon icon="fa-solid fa-gamepad" size="2xl"/>
-                        </div>
-                        <div class="carousel-caption">
-                            <h5>Organize your games</h5>
-                            <p>Keep track of your progress on all your games</p>
-                        </div>
-                    </div>                        
-                    <div class="carousel-item" data-bs-interval="10000">
                         <img :src="indexImagePath" class="d-block w-75 mx-auto">
                         <div class="carousel-caption">
                             <h5>Manage your games</h5>
@@ -48,7 +44,7 @@
                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
                     <span class="visually-hidden">Next</span>
                 </button>
-                </div>
+            </div>
         </div>  
         <div v-else>
             <user-lists :userlists="indexvm.userLists" :useraccounts="indexvm.userAccounts" :authsuccess="indexvm.authSuccess" :authaccounttypeid="indexvm.authAccountTypeID"></user-lists>
